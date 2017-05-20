fe24fddd5f7c2669ad69259e6259572e8cf005c6
Akrapong Patchararungruang <fengstp@ku.ac.th>
2017-05-21 06:33:12 +0700

    Squashed 'Code/zeabus_elec_ftdi_lib/' changes from 230329c..93ac62b
    93ac62b Change the base library to the open-source LibFTDI as D2xx library has problems with FT232H
    
    git-subtree-dir: Code/zeabus_elec_ftdi_lib
    git-subtree-split: 93ac62b2b0c012c68cc0a9a8920ccc0b1611ba59

--- README.md
+++ README.md
@@ -2,13 +2,25 @@
 Library for Power Distributer and Peripheral Bridge
 
 ## The base FTDI library
-The base library consists of 2 files, _libftd2xx.a_ and _ftd2xx.h_.
+The base library is an open-source _libftdi_. It requires _libusb_ and _libconfuse_. 
+Therefore, both dependencies were built and merged into the _libftdi.a_ library file. 
+This makes the final library consists of 2 layers as:
 
-### The header file, _ftd2xx.h_, was modified to conform the standard integer defined  in `stdint.h`. Therefore, the file `WinTypes.h`, included in the package, is unnecessary.
+* Physical layer consisting of _libftdi.a_ and _ftdi.h_.
+* Implementation layer consisting of _ftdi\_impl.h_ and _ftdi\_impl.cpp_.
 
-The library requires "pthread" and "dl" libraries provided by the system.
-Therefore, to build the program we need _-lpthread -ldl_ option. 
-For example `gcc main.c zblib.c -o runing libftd2xx -lpthread -ldl`.
+## Normally, Zeabus team use only Implementation layer and let the library manipulate the Physical layer.
+
+For Ubuntu16.04, we must install the _udev_ with the command `sudo apt-get install libudev-dev` first.
+The _libudev_ allow us to access the USB bus from user space with normal user privilege instead of `root`.
+A rule file must be created in _/etc/udev/rules.d_ to allow that. However, we already provide the rule file under the directory _udev-rules_.
+To install it, we just copy the files inside it to _/etc/udev/rules.d_ and restart the system.
+
+The library requires "pthread" and "udev" libraries provided by the system.
+Therefore, to build the program, we need _-lpthread -ludev_ option. 
+For example `gcc main.c zblib.c -o runing libftd2xx -lpthread -ldev`.
+
+
 
 ## Zeabus Library
 Zebus library was built over the base FTDI library. It provides high-level functions to control various components via FTxxx chips.
@@ -17,13 +29,11 @@ The components under control of FTxx are:
 
 * _FT232H_ The chip must be pre-programmed its serial number as "PowerDist".
   * 8-Channel galvanic-isolated power switches assigned as:
-    * 6 channels for thrusters, DVL, and Sonar (including spared channels) with directed connection to batteries
+    * 5 channels for thrusters, DVL, and Sonar (including spared channels) with directed connection to batteries
     * 1 channel for head lamps with 12V supply
     * 1 channel for DSP board with 5V supply
+    * 1 reserved channel
+
 * _FT4232H_ This chip consists of 4 independent modules Their serial numbers must be pre-programmed as "PerpheralBridge-A", "PerpheralBridge-B", "PerpheralBridge-C", and "PerpheralBridge-D".
   * 8-Channel galvanic-isolated switches for pneumatic valves.
   * 2-Channel galvanic-isolated RS232 for communication with DSP and DVL boards
-
-The library consists of 2 files, _zblib.h_ and _zblib.c_. Users should consider only information in the _zblib.h_ file.
-
-Normally, Zeabus team use only this Zeabus library and let the library manipulate base FTDI function calls.
--- ftdi_impl.h
+++ ftdi_impl.h
@@ -2,88 +2,91 @@
 #define __FTDI_IMPL_H
 
 #include <stdint.h>
-#include "ftd2xx.h"
+#include <vector>
+#include <string>
+#include <memory>
+#include "ftdi.h"
 
 /*=====================================================================================*/
-/* This HPP file and its corresponding CPP file are the thin-layer implementation of   */
+/* This H file and its corresponding CPP file are the thin-layer implementation of     */
 /* low level operation with FTDI chips. They are central operations for all FTDI chips */
 /*=====================================================================================*/
 
-const uint32_t	ulDefaultBaudRate = 115200;
-
 namespace Zeabus_Elec
 {
+
+	/* Global constant */
+	const uint32_t	ulDefaultBaudRate = 115200;
+	const uint16_t	VendorID = 0x0403;	/* VendorID ID for all FTDI chips */
+	
+	enum ChipType
+	{
+		FT232 = 0x6001,
+		FT2232 = 0x6010,
+		FT4232H = 0x6011,
+		FT232H = 0x6014
+	};
+	
+	enum ErrorCode
+	{
+		ERR_INIT_FAILED = -1001,
+		ERR_DISCOVERY_FAILED = -1002,
+		ERR_OPEN_FAILED = -1003,
+		ERR_UNSUPPORTED_DEVICE = -1004,
+		ERR_MPSSE_SYNC_FAILED = -1005,
+		ERR_GPIO_FAILED = -1006,
+		ERR_COMMUNICATION_FAILED = -1007,
+		ERR_UNSUPPORTED_FUNCTION = -1008
+	};
 
 	/* The base class used for all implementation of FTDI modes */
 	class ftdi_impl
 	{
 	public:
-		ftdi_impl( FT_DEVICE xFTDevice, FT_HANDLE xHandle ) 
-			: xFTDevice_(xFTDevice), xHandle_(xHandle), xCurrentStatus_( FT_OK ) {};
+		ftdi_impl( enum ChipType xChipID, std::string stDeviceDesc, int iIndex = 0 );
+		virtual ~ftdi_impl();
 
-		FT_STATUS GetCurrentStatus() { return xCurrentStatus_; };
-		void ClearError() { xCurrentStatus_ = FT_OK; };
+		int GetCurrentStatus() { return xCurrentStatus_; };
+		void ClearError() { xCurrentStatus_ = 0; };
 		
 		/* Abstract method */
 		/* Send data to communication channel */
-		virtual uint32_t Send( uint8_t* , uint32_t ) = 0;
+		virtual uint32_t Send( const std::vector<uint8_t>& ) = 0;
 		/* Receive data from communication channel */
-		virtual uint32_t Receive( uint8_t* , uint32_t ) = 0;
+		virtual uint32_t Receive( std::vector<uint8_t>& ) = 0;
 		/* Set I/O direction of GPIO pins (if available). The parameter is the mask. 0 = Input, 1 = Output */
-		virtual FT_STATUS SetGPIODirection( uint16_t ) = 0;
+		virtual int SetGPIODirection( uint16_t ) = 0;
 		/* Out data to high-byte GPIO pins */
-		virtual FT_STATUS SetHiGPIOData( uint8_t ) = 0;
+		virtual int SetHiGPIOData( uint8_t ) = 0;
 		/* Out data to low-byte GPIO pins */
-		virtual FT_STATUS SetLoGPIOData( uint8_t ) = 0;
+		virtual int SetLoGPIOData( uint8_t ) = 0;
 		/* Read data from high-byte GPIO pins */
 		virtual uint8_t ReadHiGPIOData() = 0;
 		/* Read data from low-byte GPIO pins */
 		virtual uint8_t ReadLoGPIOData() = 0;
 		
-		/* Get the amount of data in Rx Buffer. These data should be read out by FT_Read later. */
-		uint32_t CheckWaitingData()
-		{
-			uint32_t ulAmountData;
-			
-			if( xCurrentStatus_ != FT_OK )
-			{
-				return( 0 );
-			}
-
-			/* Check the receive buffer - it should contain the data */
-			xCurrentStatus_ = FT_GetQueueStatus( xHandle_, &ulAmountData );
-	
-			if( xCurrentStatus_ != FT_OK )
-			{
-				ulAmountData = 0;
-			}
-	
-			return( ulAmountData );
-		};
-
 	protected:
-		FT_HANDLE xHandle_;
-		FT_DEVICE xFTDevice_;
-		FT_STATUS xCurrentStatus_;
+		struct ftdi_context* xHwContext_;
+		int xCurrentStatus_;
 	};
 	/*===================================================================*/
 
-	/* Basic implementation class for MSSP mode without any particular protocol (e.g. SPI, I2C). */
-	class ftdi_mssp_impl : public ftdi_impl
+	/* Basic implementation class for MPSSE mode without any particular protocol (e.g. SPI, I2C). */
+	class ftdi_mpsse_impl : public ftdi_impl
 	{
 	public:
-		ftdi_mssp_impl( FT_DEVICE xFTDevice, FT_HANDLE xHandle );
+		ftdi_mpsse_impl( enum ChipType xChipID, std::string stDeviceDesc, int iIndex = 0 );
 		
 		/* Send data to communication channel */
-		virtual uint32_t Send( uint8_t* pucData, uint32_t ulLen );
+		virtual uint32_t Send( const std::vector<uint8_t>& pucData );
 		/* Receive data from communication channel */
-		virtual uint32_t Receive( uint8_t* pucData, uint32_t ulMaxLen );
+		virtual uint32_t Receive( std::vector<uint8_t>& pucData );
 		/* Set I/O direction of GPIO pins (if available). The parameter is the mask. 0 = Input, 1 = Output */
-		virtual FT_STATUS SetGPIODirection( uint16_t usData);
+		virtual int SetGPIODirection( uint16_t usData);
 		/* Out data to high-byte GPIO pins */
-		virtual FT_STATUS SetHiGPIOData( uint8_t ucData );
+		virtual int SetHiGPIOData( uint8_t ucData );
 		/* Out data to low-byte GPIO pins */
-		virtual FT_STATUS SetLoGPIOData( uint8_t ucData );
+		virtual int SetLoGPIOData( uint8_t ucData );
 		/* Read data from high-byte GPIO pins */
 		virtual uint8_t ReadHiGPIOData();
 		/* Read data from low-byte GPIO pins */
@@ -98,27 +101,25 @@ namespace Zeabus_Elec
 	class ftdi_uart_impl : public ftdi_impl
 	{
 	public:
-		ftdi_uart_impl( FT_DEVICE xFTDevice, FT_HANDLE xHandle, uint32_t ulBaudRate = ulDefaultBaudRate );
+		ftdi_uart_impl( enum ChipType xChipID, std::string stDeviceDesc, int iIndex = 0 , uint32_t ulBaudRate = ulDefaultBaudRate);
 		
 		/* Change baud rate */
-		FT_STATUS SetBaudRate( uint32_t ulBaudRate );
+		int SetBaudRate( uint32_t ulBaudRate );
 		
 		/* Send data to communication channel */
-		virtual uint32_t Send( uint8_t* pucData, uint32_t ulLen );
+		virtual uint32_t Send( const std::vector<uint8_t>& pucData );
 		/* Receive data from communication channel */
-		virtual uint32_t Receive( uint8_t* pucData, uint32_t ulMaxLen );
+		virtual uint32_t Receive( std::vector<uint8_t>& pucData );
 		/* Set I/O direction of GPIO pins (if available). The parameter is the mask. 0 = Input, 1 = Output */
-		virtual FT_STATUS SetGPIODirection( uint16_t );
+		virtual int SetGPIODirection( uint16_t );
 		/* Out data to high-byte GPIO pins */
-		virtual FT_STATUS SetHiGPIOData( uint8_t );
+		virtual int SetHiGPIOData( uint8_t );
 		/* Out data to low-byte GPIO pins */
-		virtual FT_STATUS SetLoGPIOData( uint8_t );
+		virtual int SetLoGPIOData( uint8_t );
 		/* Read data from high-byte GPIO pins */
 		virtual uint8_t ReadHiGPIOData();
 		/* Read data from low-byte GPIO pins */
 		virtual uint8_t ReadLoGPIOData();
-	
-	protected:
 	};
 	/*===================================================================*/
 	
@@ -129,21 +130,19 @@ namespace Zeabus_Elec
 	     	and by device at the second/first edge of clock (rising or falling edge depends on
 	     	the clock polarity (CPOL)
 	     - MSB/LSB => Data are communicated with MSB first or LSB first. */
-	class ftdi_spi_impl : public ftdi_mssp_impl
+	class ftdi_spi_impl : public ftdi_mpsse_impl
 	{
 	public:
-		ftdi_spi_impl( FT_DEVICE xFTDevice, FT_HANDLE xHandle );
+		ftdi_spi_impl( enum ChipType xChipID, std::string stDeviceDesc, int iIndex = 0 );
 		
 		/* Send data to communication channel */
-		virtual uint32_t Send( uint8_t* pucData, uint32_t ulLen );
+		virtual uint32_t Send( const std::vector<uint8_t>& pucData );
 		/* Receive data from communication channel */
-		virtual uint32_t Receive( uint8_t* pucData, uint32_t ulMaxLen );
+		virtual uint32_t Receive( std::vector<uint8_t>& pucData );
 		/* Set I/O direction of GPIO pins (if available). The parameter is the mask. 0 = Input, 1 = Output */
-		virtual FT_STATUS SetGPIODirection( uint16_t usData);
+		virtual int SetGPIODirection( uint16_t usData);
 		/* Out data to low-byte GPIO pins */
-		virtual FT_STATUS SetLoGPIOData( uint8_t ucData );
-		/* Read data from low-byte GPIO pins */
-		virtual uint8_t ReadLoGPIOData();
+		virtual int SetLoGPIOData( uint8_t ucData );
 	
 	protected:
 		const uint16_t SPIMaskOut_ = 0xFFF0;
@@ -158,7 +157,7 @@ namespace Zeabus_Elec
 	class ftdi_spi_cpol1_cha0_msb_impl : public ftdi_spi_impl
 	{
 	public:
-		ftdi_spi_cpol1_cha0_msb_impl( FT_DEVICE xFTDevice, FT_HANDLE xHandle );
+		ftdi_spi_cpol1_cha0_msb_impl( enum ChipType xChipID, std::string stDeviceDesc, int iIndex = 0 );
 		
 	protected:
 		virtual const uint16_t GetSPIIOMask() { return( 0xFFFB ); };
